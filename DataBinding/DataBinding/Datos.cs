9da5a7ead096ba8bb6e757cedf4ade0f5f50d686
ManuelMatheuz03 <38057638+ManuelMatheuz03@users.noreply.github.com>
2018-05-08 21:20:48 -0500

    Publicar Binding

--- DataBinding/DataBinding/Datos.cs
+++ DataBinding/DataBinding/Datos.cs
@@ -8,9 +8,26 @@ namespace DataBinding
     class Datos:Notificable
     {
         private ObservableCollection<Persona> personas;
+        private Persona personaSeleccionada;
 
         #region Propiedades
+        public Persona PersonaSeleccionada
+        {
+            get
+            {
+                return personaSeleccionada;
+            }
 
+            set
+            {
+                if (personaSeleccionada == value)
+                {
+                    return;
+                }
+                personaSeleccionada = value;
+                OnPropertyChanged();
+            }
+        }
         public ObservableCollection<Persona> Personas
         {
             get { return personas; }
@@ -30,7 +47,7 @@ namespace DataBinding
         public Datos()
         {
             Personas = new ObservableCollection<Persona>();
-            for (int i = 0; i < 6; i++)
+            for (int i = 1; i < 6; i++)
             {
                 Personas.Add(new Persona()
                 {
